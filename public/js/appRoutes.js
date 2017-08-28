e2060cf2d9f138443bc9dec04ed49191755f7a64
Aditya <adityaverma2010@hotmail.com>
2017-08-29 02:20:31 +0530

    Profile Update

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,23 +1,25 @@
-angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {
 
-	$routeProvider
+		$routeProvider
 
 		// home page
-		.when('/', {
+			.when('/', {
 			templateUrl: 'views/home.html',
 			controller: 'MainController'
 		})
 
-		.when('/nerds', {
-			templateUrl: 'views/nerd.html',
+		.when('/about', {
+			templateUrl: 'views/about.html',
 			controller: 'NerdController'
 		})
 
 		.when('/geeks', {
 			templateUrl: 'views/geek.html',
-			controller: 'GeekController'	
+			controller: 'GeekController'
 		});
 
-	$locationProvider.html5Mode(true);
+		$locationProvider.html5Mode(true);
 
-}]);
+	}
+]);
